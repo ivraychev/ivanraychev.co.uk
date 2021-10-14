036c67af75ebed89c4dd26c27fb15017bd0ff872
ivraychev <81635350+ivraychev@users.noreply.github.com>
2021-10-14 16:34:25 +0100

    Update index.js

--- project3/weather/js/index.js
+++ project3/weather/js/index.js
@@ -2,7 +2,6 @@ const form = document.querySelector(".top-banner form");
 const input = document.querySelector(".top-banner input");
 const msg = document.querySelector(".top-banner .msg");
 const list = document.querySelector(".ajax-section .cities");
-/*SUBSCRIBE HERE FOR API KEY: https://home.openweathermap.org/users/sign_up*/
 const apiKey = "4d8fb5b93d4af21d66a2948710284366";
 
 form.addEventListener("submit", e => {
